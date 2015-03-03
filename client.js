402d6b41f6a994d08b2683e442bf691901c8fabd
Brett Porter <brett.porter@safetyculture.io>
2015-03-03 16:36:22 +1100

    support simultaneous requests

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var async = require('async');
 
 var url = process.argv[2];
 var method = process.argv[3];
@@ -13,70 +14,85 @@ if (method === undefined) {
 }
 
 var partialWrite = process.env['PARTIAL_WRITE'] || false;
+var numRequests = process.env['NUM_REQUESTS'] || 1;
+var maxSockets = process.env['MAX_SOCKETS'];
 
-var options = require('url').parse(url);
-options.method = method;
+if (maxSockets !== undefined) {
+  http.globalAgent.maxSockets = maxSockets;
+}
 
-console.log("Requesting", method, url);
-request = http.request(options, function(response) {
-  response.on('end', function() {
-    console.log("RESPONSE END");
+var doRequest = function(count, callback) {
+  var options = require('url').parse(url);
+  options.method = method;
+
+  console.log("Requesting", count, method, url);
+  request = http.request(options, function(response) {
+    response.on('end', function() {
+      console.log(count, "RESPONSE END");
+      callback(null);
+    })
+
+    response.on('error', function(error) {
+      console.error(count, "RESPONSE ERROR", error);
+    })
+
+    console.log(count, "Code", response.statusCode);
+
+    if (method == 'GET') {
+      console.log(count, "Saving to client-outfile.tmp...");
+      var stream = fs.createWriteStream('client-outfile.tmp');
+      response.pipe(stream);
+    }
+    else {
+      response.pipe(process.stdout);
+    }
+  });
+
+  request.on('close', function() {
+    console.log(count, "REQUEST CLOSE");
   })
 
-  response.on('error', function(error) {
-    console.error("RESPONSE ERROR", error);
+  request.on('end', function() {
+    console.log(count, "REQUEST END");
   })
 
-  console.log("Code", response.statusCode);
+  // Alternative for timeout, same thing:
+  // request.on('socket', function(socket) {
+  //   socket.setTimeout(1000);
+  //   socket.on('timeout', function(timeout) {
+  //     console.log("SOCKET TIMEOUT");
+  //     socket.destroy();
+  //   })
+  // })
 
-  if (method == 'GET') {
-    console.log("Saving to client-outfile.tmp...");
-    var stream = fs.createWriteStream('client-outfile.tmp');
-    response.pipe(stream);
+  request.setTimeout(10000);
+  request.on('timeout', function() {
+    console.log(count, "REQUEST TIMEOUT");
+    request.abort();
+  })
+
+  request.on('error', function(error) {
+    console.error(count, "REQUEST ERROR", error);
+    callback(null);
+  })
+
+  if (method == 'PUT') {
+    if (partialWrite) {
+      console.log(count, "Partial write...");
+      request.write("ready...");
+      // no request.end();
+    } else {
+      console.log(count, "Posting bigfile.img...");
+      var stream = fs.createReadStream('bigfile.img');
+      stream.pipe(request);
+    }
   }
   else {
-    response.pipe(process.stdout);
+    request.end();
   }
+}
+
+// TODO: limit concurrent
+async.times(numRequests, doRequest, function(err, results) {
+  console.log("DONE");
 });
-
-request.on('close', function() {
-  console.log("REQUEST CLOSE");
-})
-
-request.on('end', function() {
-  console.log("REQUEST END");
-})
-
-// Alternative for timeout, same thing:
-// request.on('socket', function(socket) {
-//   socket.setTimeout(1000);
-//   socket.on('timeout', function(timeout) {
-//     console.log("SOCKET TIMEOUT");
-//     socket.destroy();
-//   })
-// })
-
-request.setTimeout(10000);
-request.on('timeout', function() {
-  console.log("REQUEST TIMEOUT");
-  request.abort();
-})
-
-request.on('error', function(error) {
-  console.error("REQUEST ERROR", error);
-})
-
-if (method == 'PUT') {
-  if (partialWrite) {
-    console.log("Partial write...");
-    request.write("ready...");
-    // no request.end();
-  } else {
-    console.log("Posting bigfile.img...");
-    var stream = fs.createReadStream('bigfile.img');
-    stream.pipe(request);
-  }
-}
-else {
-  request.end();
-}
